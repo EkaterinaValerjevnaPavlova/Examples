cd4400635f1c5c6acd248799c8a8181461405b3b
Ekaterina Pavlova <katja197@ya.ru>
2022-11-14 10:23:04 +0300

    Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {5,54,5487,2,67,5,13};
+
+int n = array.Length;
+int find = 67;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
